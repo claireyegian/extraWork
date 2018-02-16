852ebceb28019e544b29bc7b2a8846b522892e8e
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-02-16 14:29:04 -0700

    Updated from Brython Server: 2/16/2018 2:29:19 PM

--- battleship.py
+++ battleship.py
@@ -16,13 +16,37 @@ def redrawAll():
         columnNum = 0
         for column in row:
             if data['gameBoard'][rowNum][columnNum] == 0:
-                """Sprite(data['deadCell'],(columnNum*50,rowNum*50))"""
+                Sprite(data['empty'],(columnNum*50,rowNum*50))
             if data['gameBoard'][rowNum][columnNum] == 1:
-                """Sprite(data['liveCell'],(columnNum*50,rowNum*50))"""
+                Sprite(data['miss'],(columnNum*50,rowNum*50))
             if data['gameBoard'][rowNum][columnNum] == 2:
-                DO THING
+                Sprite(data['hit'],(columnNum*50,rowNum*50))
             columnNum += 1
         rowNum += 1
 
-pickComputerShips():
+"""pickComputerShips():
     rowShip1 = randint(1,9)
+
+def mouseClick(event):
+    if data['numShips'] == 3"""
+
+if __name__ == '__main__':
+    empty = Color(0xffffff,1) #Colors used in program
+    miss = Color(0xD3D3D3,1)
+    hit = Color(0xff0000,1)
+    
+    data = {} #Program dictionary
+    data['empty'] = RectangleAsset(50,50,LineStyle(1,miss),empty)
+    data['miss'] = RectangleAsset(50,50,LineStyle(1,miss),miss)
+    data['hit'] = RectangleAsset(50,50,LineStyle(1,hit),hit)
+    data['numShips'] = 0
+    data['gameBoardUpdate'] = []
+    
+    data['gameBoard'] = gameBoard()
+    redrawAll()
+    
+    """nextGen = TextAsset('NextGeneration')
+    Sprite(nextGen,(190,520))
+    
+    App().listenMouseEvent('click',mouseClick)"""
+    App().run()
