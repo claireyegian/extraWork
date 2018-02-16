1e28d671a601ca3b695525b78f830b1694d365ff
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-02-16 14:46:26 -0700

    Updated from Brython Server: 2/16/2018 2:46:25 PM

--- battleship.py
+++ battleship.py
@@ -21,6 +21,8 @@ def redrawAll():
                 Sprite(data['miss'],(columnNum*50,rowNum*50))
             if data['gameBoard1'][rowNum][columnNum] == 2:
                 Sprite(data['hit'],(columnNum*50,rowNum*50))
+            if data['gameBoard2'][rowNum][columnNum] == 3:
+                Sprite(data['ship'],(columnNum*50,rowNum*50))
             columnNum += 1
         rowNum += 1
     rowNum = 0
@@ -33,27 +35,33 @@ def redrawAll():
                 Sprite(data['miss'],((columnNum*50)+550,rowNum*50))
             if data['gameBoard2'][rowNum][columnNum] == 2:
                 Sprite(data['hit'],((columnNum*50)+550,rowNum*50))
+            if data['gameBoard2'][rowNum][columnNum] == 3:
+                Sprite(data['ship'],((columnNum*50)+550,rowNum*50))
             columnNum += 1
         rowNum += 1
 
 """pickComputerShips():
-    rowShip1 = randint(1,9)
+    rowShip1 = randint(1,9)"""
 
 def mouseClick(event):
     row = event.y//50
     column = event.x//50
     if data['numShips'] < 3:
-        data['gameBoard'][row][column] == """
+        data['gameBoard1'][row][column] == 3
+        data['numShips'] += 1
+    
 
 if __name__ == '__main__':
     empty = Color(0xffffff,1) #Colors used in program
     miss = Color(0xD3D3D3,1)
     hit = Color(0xff0000,1)
+    ship = Color(0x000000,1)
     
     data = {} #Program dictionary
     data['empty'] = RectangleAsset(50,50,LineStyle(1,miss),empty)
     data['miss'] = RectangleAsset(50,50,LineStyle(1,miss),miss)
     data['hit'] = RectangleAsset(50,50,LineStyle(1,hit),hit)
+    data['ship'] = RectangleAsset(50,50,LineStyle(1,ship),ship)
     data['numShips'] = 0
     data['gameBoardUpdate'] = []
     
