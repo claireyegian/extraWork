feb45b2cb4ddfdf1c4057adfa59f7f1bce8744c5
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-02-28 15:41:58 -0500

    Updated from Brython Server: 2/28/2018 3:41:57 PM

--- othello.py
+++ othello.py
@@ -2,6 +2,8 @@
 #2/28/18
 #othello.py - creates othello game board
 
+from ggame import *
+
 #Creates gameboard
 def buildBoard():
     return [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,1,2,0,0,0],[0,0,0,2,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
@@ -10,30 +12,33 @@ def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
     rowNum = 0
-    for row in data['gameBoard1']:
+    for row in data['gameBoard']:
         columnNum = 0
         for column in row:
-            if data['gameBoard1'][rowNum][columnNum] == 0:
+            if data['gameBoard'][rowNum][columnNum] == 0:
                 Sprite(data['empty'],(columnNum*50,rowNum*50))
-            if data['gameBoard1'][rowNum][columnNum] == 1:
+            if data['gameBoard'][rowNum][columnNum] == 1:
+                Sprite(data['empty'],(columnNum*50,rowNum*50))
                 Sprite(data['player'],(columnNum*50,rowNum*50))
-            if data['gameBoard1'][rowNum][columnNum] == 2:
+            if data['gameBoard'][rowNum][columnNum] == 2:
+                Sprite(data['empty'],(columnNum*50,rowNum*50))
                 Sprite(data['computer'],(columnNum*50,rowNum*50))
             columnNum += 1
         rowNum += 1
 
 if __name__ == '__main__':
     computer = Color(0xffffff,1) #Colors used in program
-    empty = Color(0xDA70D6,1)
+    empty = Color(0x7CACE4,1)
     player = Color(0x000000,1)
+    boarder = Color(0x5788C0,1)
     
     data = {} #Program dictionary
-    data['empty'] = RectangleAsset(50,50,LineStyle(1,empty),empty)
-    data['computer'] = CircleAsset(50,50,LineStyle(1,computer),computer)
-    data['player'] = CircleAsset(50,50,LineStyle(1,player),player)
+    data['empty'] = RectangleAsset(50,50,LineStyle(1,boarder),empty)
+    data['computer'] = CircleAsset(24,LineStyle(1,boarder),computer)
+    data['player'] = CircleAsset(24,LineStyle(1,boarder),player)
 
     data['gameBoard'] = buildBoard()
     redrawAll()
     
-    App().listenMouseEvent('click',mouseClick)
+    """App().listenMouseEvent('click',mouseClick)"""
     App().run()
