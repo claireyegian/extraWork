a8ddc685941e14ad2b518961291659c6a83742f6
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-03-13 15:15:27 -0400

    Updated from Brython Server: 3/13/2018 3:15:26 PM

--- othello.py
+++ othello.py
@@ -88,10 +88,23 @@ def flipEast(row,column):
             break
         
 
-"""def flipWest(row,column):
-    
+def flipWest(row,column):
+    column -= 1
+    currentState = data['gameBoard'][row][column+1]
+    flipNum = 0
+    while column > 0:
+        if data['gameBoard'][row][column] != 0 and data['gameBoard'][row][column] != currentState:
+            flipNum += 1
+            column -= 1
+        elif data['gameBoard'][row][column] == currentState:
+            if flipNum > 0:
+                print('flip')
+            elif flipNum == 0:
+                break
+        elif data['gameBoard'][row][column] == 0:
+            break
 
-def flipNorth(row,column):
+"""def flipNorth(row,column):
     
 
 def flipSouth(row,column):
